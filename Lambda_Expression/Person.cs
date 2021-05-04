66ec414d0e4e37474c917e68dee2d1c2c1cc1c6b
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-04 09:47:19 +0530

    Add project files.

---
+++ Lambda_Expression/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpression
+{
+    class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+        public Person(string ssn, string name, string addr, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = addr;
+            Age = age;
+        }
+    }
+
+}
